992bd50f400d13e6a87a65ef30b98dc84854495c
Jpetitgas <jpetitgas63@gmail.com>
2021-03-10 21:38:09 +0100

    update tthe app to 4.4.20

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -29,7 +29,7 @@ class UserController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $password = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
@@ -54,7 +54,7 @@ class UserController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $password = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
 
