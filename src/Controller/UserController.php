7d0cb961c0b8bca77e645414cd09a1390f4d4fd5
Jpetitgas <jpetitgas63@gmail.com>
2021-03-16 20:07:27 +0100

    add user test (create, edit and delete)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -12,18 +15,25 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
+    
+    
     /**
      * @Route("/users", name="user_list")
      */
-    public function listAction()
+    public function listUsers(UserRepository $userRepository)
     {
-        return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('App:User')->findAll()]);
+        $users = $userRepository->findAll();
+
+        return $this->render(
+            'user/list.html.twig',
+            ['users' => $users]
+        );
     }
 
     /**
      * @Route("/users/create", name="user_create")
      */
-    public function createAction(Request $request, UserPasswordEncoderInterface $encoder)
+    public function createUser(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -49,8 +59,14 @@ class UserController extends AbstractController
     /**
      * @Route("/users/{id}/edit", name="user_edit")
      */
-    public function editAction(User $user, Request $request, UserPasswordEncoderInterface $encoder)
+    public function editUser($id,Request $request, UserPasswordEncoderInterface $encoder,UserRepository $userRepository)
     {
+        $user = $userRepository->find($id);
+        if (!($user)) {
+            $this->addFlash('error', "L'utilisateur n'existe pas");
+            return $this->RedirectToRoute('user_list');
+        }
+        
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
@@ -67,5 +83,21 @@ class UserController extends AbstractController
         }
 
         return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+    /**
+     * @Route("/users/{id}/delete", name="user_delete")
+     */
+    public function deleteUser($id, Request $request,UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        $user = $userRepository->find($id);
+        if (!($user)) {
+            $this->addFlash('error', "L'utilisateur n'existe pas");
+            return $this->RedirectToRoute('user_list');
+        }
+       
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('success', "L'utilisateur a bien été supprimé");
+        return $this->RedirectToRoute('user_list');
     }
 }
