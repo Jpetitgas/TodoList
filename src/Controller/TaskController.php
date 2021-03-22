9d5b0617dcb3f1e2af903c009f19c1218d6b113e
Jpetitgas <jpetitgas63@gmail.com>
2021-03-22 09:13:53 +0100

    remove action suffixe

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -95,7 +95,7 @@ class TaskController extends AbstractController
     /**
      * @Route("/tasks/{id}/toggle", name="task_toggle")
      */
-    public function toggleTaskAction(Task $task)
+    public function toggleTask(Task $task)
     {
         $task->toggle(!$task->isDone());
         $this->getDoctrine()->getManager()->flush();
